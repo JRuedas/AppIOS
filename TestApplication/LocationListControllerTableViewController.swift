d382fd6ebf8d4fa3b9570b8aa2544d2bd1471b0e
Jonathan Mora <jonathanruedasmora@imac-de-cblanco.local>
2017-06-21 10:17:21 +0200

    Initial Commit

---
+++ TestApplication/LocationListControllerTableViewController.swift
@@ -0,0 +1,108 @@
+//
+//  LocationListControllerTableViewController.swift
+//  TestApplication
+//
+//  Created by Jonathan Ruedas Mora on 20/6/17.
+//  Copyright © 2017 Jonathan Ruedas Mora. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class LocationListControllerTableViewController: UITableViewController {
+
+    var miTabla: UITableView!
+    
+    var arrayCoord: [CLLocationCoordinate2D]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+
+        miTabla.register(UITableViewCell.self, forCellReuseIdentifier: "Celda")
+        
+        miTabla.delegate = self;
+        miTabla.dataSource = self;
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return self.arrayCoord.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Celda", for: indexPath) as UITableViewCell
+
+        let long = self.arrayCoord[indexPath.row].longitude
+        let lat = self.arrayCoord[indexPath.row].latitude
+
+        
+        cell.textLabel?.text = "Long: \(long) Lat: \(lat)"
+        
+        return cell
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TestApplication/ViewController.swift
@@ -0,0 +1,113 @@
+//
+//  ViewController.swift
+//  TestApplication
+//
+//  Created by Jonathan Ruedas Mora on 19/6/17.
+//  Copyright © 2017 Jonathan Ruedas Mora. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    
+    @IBOutlet weak var miBoton: UIButton!
+    
+    @IBOutlet weak var miLabel: UILabel!
+
+    @IBOutlet weak var miLabel2: UILabel!
+    
+    @IBOutlet weak var miBoton2: UIButton!
+    
+    var arrayCoord: [CLLocationCoordinate2D]!
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var clLocMan: CLLocationManager!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        clLocMan = CLLocationManager();
+        clLocMan.delegate = self;
+        clLocMan.desiredAccuracy = kCLLocationAccuracyKilometer;
+        arrayCoord = []     }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func acciondelboton(_ sender: Any) {
+        let clAuthStatus: CLAuthorizationStatus = CLLocationManager.authorizationStatus();
+        var str: String;
+        
+        switch clAuthStatus {
+        case .restricted:
+            str = "Restringido";
+            break;
+        case .denied:
+            str = "Denegado";
+            break;
+        case .notDetermined:
+            str = "Se van a solicitar permisos";
+            clLocMan.requestWhenInUseAuthorization();
+            break;
+        case .authorizedAlways:
+            str = "Autorizado siempre";
+            break;
+        default:
+            str = "Autorizado en uso";
+            getLocation();
+        }
+        
+        miLabel.text = str;
+        
+    }
+    
+    @IBAction func paraLectura(_ sender: Any) {
+        miLabel2.text = "Se han parado las lecturas";
+        clLocMan.stopUpdatingLocation();
+    }
+    
+    func getLocation(){
+        if(!CLLocationManager.locationServicesEnabled()){
+            miLabel2.text = "Localizacion desactivada";
+            return;
+        }
+        
+        clLocMan.startUpdatingLocation();
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let clCoordinate: CLLocationCoordinate2D = locations[0].coordinate;
+        miLabel2.text = "Longitud: \(clCoordinate.longitude) \n Latitud: \(clCoordinate.latitude)";
+        
+        let region = MKCoordinateRegion(center: clCoordinate, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+
+        mapView.setRegion(region, animated: true);
+        
+        arrayCoord.append(clCoordinate)
+        
+        let miPos: MKPointAnnotation = MKPointAnnotation()
+        miPos.coordinate = CLLocationCoordinate2DMake(clCoordinate.latitude, clCoordinate.longitude);
+        miPos.title = "Tu posicion"
+        
+        mapView.addAnnotation(miPos)
+        
+        if(arrayCoord.count>=5){
+            clLocMan.stopUpdatingLocation();
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "locationList" {
+            if let destino = segue.destination as? LocationListControllerTableViewController {
+                destino.arrayCoord = self.arrayCoord
+            }
+        }
+    }
+}
+
